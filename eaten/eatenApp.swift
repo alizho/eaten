b22fa9723174b00b78295f04035285e97dfe9780
Alicia Zhou <zhou1447@purdue.edu>
2026-06-23 21:48:05 -0500

    Initial Commit

---
+++ eaten/eatenApp.swift
@@ -0,0 +1,17 @@
+//
+//  eatenApp.swift
+//  eaten
+//
+//  Created by Alicia Zhou on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct eatenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
